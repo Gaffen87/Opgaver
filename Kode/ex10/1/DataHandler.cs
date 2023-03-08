3ae0007ac8b73024decfb2ff017f883c4769ae70
Aske Lysgaard <ably54078@edu.ucl.dk>
2023-03-08 13:30:47 +0100

    ex10

---
+++ Kode/ex10/1/DataHandler.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistens
+{
+    public class DataHandler
+    {
+        private string _dataFileName;
+            public string DataFileName
+            { 
+                get { return _dataFileName; } 
+            }
+
+        public DataHandler(string dataFileName)
+        {
+            _dataFileName = dataFileName;
+        }
+
+        public void SavePerson(Person person)
+        {
+            StreamWriter Save = new StreamWriter(_dataFileName);
+            Save.WriteLine(person.MakeTitle());
+            Save.Close();
+        }
+
+        public Person LoadPerson()
+        {
+            StreamReader Load = new StreamReader(_dataFileName);
+            string str = Load.ReadToEnd();
+
+            string[] split = str.Split(";");
+
+            Person person = new Person(split[0], DateTime.Parse(split[1]), double.Parse(split[2]), bool.Parse(split[3]), int.Parse(split[4]));
+
+            return person;
+        }
+    }
+}
---
+++ Kode/ex10/1/Person.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistens
+{
+    public class Person
+    {
+        private string _name;
+        public string Name
+            { 
+                get { return _name; } 
+                set { _name = value; } 
+            }
+
+        private DateTime _birthDate;
+            public DateTime BirthDate
+            { 
+                get { return _birthDate; }
+                set { _birthDate = value; } 
+            }
+
+        private double _height;
+            public double Height
+            { 
+                get { return _height; } 
+                set { _height = value; } 
+            }
+
+        private bool _isMarried;
+            public bool IsMarried
+            {
+                get { return _isMarried; }
+                set { _isMarried = value; }
+            }
+
+        private int _noOfChildren;
+            public int NoOfChildren
+            {
+                get { return _noOfChildren; }
+                set { _noOfChildren = value; }
+            }
+
+        public Person(string name, DateTime birthDate, double height, bool isMarried, int noOfChildren)
+        {
+            _name = name;
+            _birthDate = birthDate;
+            _height = height;
+            _isMarried = isMarried;
+            _noOfChildren = noOfChildren;
+        }
+
+        public string MakeTitle()
+        {
+            return $"{_name};{_birthDate};{_height};{_isMarried};{_noOfChildren}";
+        }
+    }
+}
