8b2f61acf4323a8460541512857382fdb474d54a
Aske Lysgaard <ably54078@edu.ucl.dk>
2023-05-04 10:08:50 +0200

    ex27 færdig

---
+++ Kode/WPFAndMVVM2-udleveret/WPFAndMVVM2/ViewModels/MainViewModel.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+using WPFAndMVVM2.Models;
+
+namespace WPFAndMVVM2.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+
+		private PersonRepository personRepo = new PersonRepository();
+
+		public ObservableCollection<PersonViewModel> PersonsVM { get; set; }
+
+        private PersonViewModel selectedPerson;
+
+        public PersonViewModel SelectedPerson
+        {
+            get { return selectedPerson; }
+            set 
+            {
+                selectedPerson = value; 
+                OnPropertyChanged(nameof(SelectedPerson));
+            }
+        }
+
+        public MainViewModel()
+        {
+            PersonsVM = new ObservableCollection<PersonViewModel>();
+            foreach (var person in personRepo.GetAll()) 
+            {
+                PersonViewModel pvm = new(person);
+                PersonsVM.Add(pvm);
+            }
+        }
+
+        public void AddDefaultPerson()
+        {
+			Person person = new()
+			{
+				FirstName = "Specify FirstName",
+				LastName = "Specify LastName",
+				Age = 0,
+				Phone = "Specify Phone"
+			};
+
+			personRepo.Add(person.FirstName, person.LastName, person.Age, person.Phone);
+            PersonViewModel newPerson = new(person);
+            PersonsVM.Add(newPerson);
+            SelectedPerson = newPerson;
+        }
+
+        public void DeleteSelectedPerson()
+        {
+            SelectedPerson.DeletePerson(personRepo);
+            PersonsVM.Remove(SelectedPerson);
+        }
+
+		public event PropertyChangedEventHandler PropertyChanged;
+		protected void OnPropertyChanged(string propertyName)
+		{
+			PropertyChangedEventHandler propertyChanged = this.PropertyChanged;
+			if (propertyChanged != null)
+			{
+				propertyChanged(this, new PropertyChangedEventArgs(propertyName));
+			}
+		}
+        // Implement the rest of this MainViewModel class below to 
+        // establish the foundation for data binding !
+
+    }
+}
---
+++ Kode/WPFAndMVVM2-udleveret/WPFAndMVVM2/ViewModels/PersonViewModel.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using WPFAndMVVM2.Models;
+
+namespace WPFAndMVVM2.ViewModels
+{
+    public class PersonViewModel : INotifyPropertyChanged
+    {
+        private Person person;
+
+		public event PropertyChangedEventHandler PropertyChanged;
+		protected void OnPropertyChanged(string propertyName)
+		{
+			PropertyChangedEventHandler propertyChanged = this.PropertyChanged;
+			if (propertyChanged != null)
+			{
+				propertyChanged(this, new PropertyChangedEventArgs(propertyName));
+			}
+		}
+
+        // Properties
+        private string firstName;
+        public string FirstName
+        {
+            get { return firstName; }
+            set 
+            { 
+                firstName = value;
+                OnPropertyChanged(nameof(FirstName));
+                OnPropertyChanged(nameof(FullName));
+            }
+        }
+        private string lastName;
+        public string LastName
+        {
+            get { return lastName; }
+            set 
+            { 
+                lastName = value;
+                OnPropertyChanged(nameof(LastName));
+                OnPropertyChanged(nameof(FullName));
+            }
+        }
+        public int Age { get; set; }
+        public string Phone { get; set; }
+        public string FullName
+        {
+            get { return $"{FirstName} {LastName}"; }
+            set 
+            {
+                OnPropertyChanged(nameof(FullName));
+            }
+        }
+
+        // Constructor
+        public PersonViewModel(Person person)
+        {
+            this.person = person;
+            FirstName = person.FirstName;
+            LastName = person.LastName;
+            Age = person.Age;
+            Phone = person.Phone;
+        }
+
+        public void DeletePerson(PersonRepository repo)
+        {
+            repo.Remove(person.Id);
+        }
+    }
+}
